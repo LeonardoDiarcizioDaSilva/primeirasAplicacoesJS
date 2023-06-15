42d7d3d97680d5e53069a72275e77f85cb1624e7
LeonardoDiarcizioDaSilva <121073330+LeonardoDiarcizioDaSilva@users.noreply.github.com>
2023-06-14 21:53:05 -0300

    Criando uma maneira melhor de interagir com o jogo de adivinhar 2.0

--- adivinhar-2.0.js
+++ adivinhar-2.0.js
@@ -1,26 +1,79 @@
 function escreverNaTela(algumtexto) {
-    document.write(algumtexto);
+    alert(algumtexto);
 }
 
-var segredo = Math.round(Math.random() * 10);
+function geradorDeNumero() {
+    return Math.round(Math.random() * 10);
+}
+
+function sorteiaNumero(quantidade) {
+
+    var numeros = [];
+    var tamanhoLista = 1;
+
+    while(tamanhoLista <= quantidade) {
+
+        var numeroAleatorio = geradorDeNumero();
+        var achou = false;
+
+        if(numeroAleatorio !== 0) {
+            for(posicao = 0; posicao < numeros.length; posicao++) {
+
+                if(numeros[posicao] == numeroAleatorio) {
+
+                    achou = true;
+                    break;
+
+                }
+            }
+
+            if(achou == false) {
+
+                numeros.push(numeroAleatorio);
+                tamanhoLista++;
+            }
+        }
+    }
+
+    return numeros;
+    
+}
+
+var segredo = sorteiaNumero(2);
+console.log(segredo);
 
 var input = document.querySelector("input");
-
 input.focus();
 
 function vereficarSegredo() {
 
+    var achou = false;
+    var acertou = false;
 
-    if(input.value == segredo) {
+    for(posicao = 0; posicao <= segredo.length; posicao++) {
 
-        alert("Parabes!!! Você acertou o número.");
+        if(input.value == segredo[posicao]) {
 
-    } else {
+            escreverNaTela("Parabes!!! Você acertou o número.");
+            achou = true;
+            acertou = true;
+            break;
 
-        alert("Que pena!!! Você errou o número, tente novamente.");
-        input.value = "";
-        input.focus();
+        }
+    }
 
+    if(achou == false) {
+        escreverNaTela("Que pena!!! Você errou o número, tente novamente.");
+    }
+
+    input.value = "";
+    input.focus();
+
+    if(acertou == true) {
+        escreverNaTela("Gerando novos números.");
+
+        segredo = sorteiaNumero(2);
+        console.log(segredo);
     }
 }
 
